d67c2866f5afb97eed8e5f1d5357eff00e5ee689
swe07 <103895754+swe07@users.noreply.github.com>
2024-01-24 13:00:48 +0530

    Add files via upload

---
+++ Flask-Knee-Osteoarthritis-Classification-main/app.py
@@ -0,0 +1,48 @@
+
+
+from flask import Flask, render_template, request
+from keras.models import load_model
+from keras.preprocessing import image
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+dic = {0: 'Normal', 1: 'Doubtful', 2: 'Mild', 3: 'Moderate', 4: 'Severe'}
+
+
+img_size = 256
+model = load_model('model.h5')
+model.make_predict_function()
+
+def predict_label(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    resized = cv2.resize(gray, (img_size, img_size))
+    i = image.img_to_array(resized) / 255.0
+    i = i.reshape(1, img_size, img_size, 1)
+    predictions = model.predict(i)
+    p = np.argmax(predictions, axis=-1)
+    return dic[p[0]]
+
+# routes
+@app.route("/", methods=['GET', 'POST'])
+def main():
+    return render_template("index.html")
+
+@app.route("/about")
+def about_page():
+    return "Please subscribe Artificial Intelligence Hub..!!!"
+
+@app.route("/predict", methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        img = request.files['file']
+        img_path = "uploads/" + img.filename
+        img.save(img_path)
+        p = predict_label(img_path)
+        print(p)
+        return str(p).lower()
+
+if __name__ == '__main__':
+    app.run(debug=True)
